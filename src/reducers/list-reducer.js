e7994cbb245f2d3eab1cf7b7a66f271986fcfead
Bar Horing Amir <bar.apple@gmail.com>
2020-08-19 21:48:00 +0300

    Done create new car

--- src/reducers/list-reducer.js
+++ src/reducers/list-reducer.js
@@ -3,6 +3,29 @@ import { lists as defaultLists } from "../normalized-state";
 const listReducer = (lists = defaultLists, action) => {
   console.log("hello from listReducer");
   console.log(lists, action);
+  if (action.type === "CARD_CREATE") {
+    const { cardId, listId } = action.payload;
+    // Add the cardId to the cards array of the appropriate list
+
+    // create a shalow copy of lists.entities
+    const entities = { ...lists.entities };
+
+    // replace the list in shalow copy
+    // and add another card to the cards ids list
+    entities[listId] = {
+      ...entities[listId],
+      cards: entities[listId].cards.concat(cardId),
+      // cards: [...entities[listId].cards, cardId]
+    };
+
+    return {
+      ...lists,
+      entities,
+      // equivalet to
+      // ...entites
+      // ids: [...lists.ids],
+    };
+  }
   return lists;
 };
 
